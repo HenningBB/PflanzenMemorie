cb701d77a6016fd734411d537f02cc4d47d587ea
Jafa95 <61741048+Jafa95@users.noreply.github.com>
2020-09-11 11:30:49 +0200

    Merge branch 'newstart' of C:\Users\yaherzig\AndroidStudioProjects\PflanzenMemorie with conflicts.

--- app/src/main/java/hkbb/de/pflanzenmemorie/quizQuestion.java
+++ app/src/main/java/hkbb/de/pflanzenmemorie/quizQuestion.java
@@ -3,6 +3,7 @@ package hkbb.de.pflanzenmemorie;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,6 @@ import android.view.ViewGroup;
 
 
 public class quizQuestion extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
