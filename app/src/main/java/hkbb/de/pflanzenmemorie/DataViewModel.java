b7dc1405fa6383f0ff5c913d381772c3963ff083
HenningBB <henning.bull-berg@tsbw.cloud>
2020-09-11 10:31:28 +0200

    filled the DataViewModel

--- app/src/main/java/hkbb/de/pflanzenmemorie/DataViewModel.java
+++ app/src/main/java/hkbb/de/pflanzenmemorie/DataViewModel.java
@@ -1,4 +1,37 @@
 package hkbb.de.pflanzenmemorie;
 
-public class DataViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class DataViewModel extends ViewModel {
+    private MutableLiveData<Pflanze> Karte = new MutableLiveData<>();
+    private MutableLiveData<List<Pflanze>> Kasten = new MutableLiveData<>();
+    private MutableLiveData<List<Pflanze>> Quiz = new MutableLiveData<>();
+
+    public void setKarte(Pflanze karte) {
+        Karte.setValue(karte);
+    }
+
+    public void setKasten(List<Pflanze> kasten) {
+        Kasten.setValue(kasten);
+    }
+
+    public void setQuiz(List<Pflanze> quiz) {
+        Quiz.setValue(quiz);
+    }
+
+    public LiveData<Pflanze> getKarte() {
+        return Karte;
+    }
+
+    public LiveData<List<Pflanze>> getKasten() {
+        return Kasten;
+    }
+
+    public LiveData<List<Pflanze>> getQuiz() {
+        return Quiz;
+    }
 }
