382da55307139fc31fc16090d2f44033b61eb7e1
HenningBB <henning.bull-berg@tsbw.cloud>
2020-09-14 09:53:01 +0200

    implemented the textviews and button for the login into the java class

--- app/src/main/java/hkbb/de/pflanzenmemorie/Login.java
+++ app/src/main/java/hkbb/de/pflanzenmemorie/Login.java
@@ -2,11 +2,14 @@ package hkbb.de.pflanzenmemorie;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Login extends Fragment {
@@ -16,5 +19,19 @@ public class Login extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btn_login = view.findViewById(R.id.login_btn);
+        TextView txt_username = view.findViewById(R.id.userName_txt);
+        TextView txt_pw = view.findViewById(R.id.userPassword_txt);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
