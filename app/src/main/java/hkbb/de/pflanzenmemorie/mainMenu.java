e9f92d5a33c105ad27fa57beb513c62e4fe0bd0e
HenningBB <henning.bull-berg@tsbw.cloud>
2020-09-15 12:15:39 +0200

    it is possible to logout from the main menu now

--- app/src/main/java/hkbb/de/pflanzenmemorie/mainMenu.java
+++ app/src/main/java/hkbb/de/pflanzenmemorie/mainMenu.java
@@ -2,11 +2,14 @@ package hkbb.de.pflanzenmemorie;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class mainMenu extends Fragment {
@@ -16,5 +19,17 @@ public class mainMenu extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_menu, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btn_abmelden= view.findViewById(R.id.menuLogout_btn);
+        btn_abmelden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(mainMenu.this).navigate(R.id.action_mainMenu_to_login);
+            }
+        });
+
     }
 }
