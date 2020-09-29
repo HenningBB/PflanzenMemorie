2024ce5d771c3613d92c33430d718e24f60588ae
HenningBB <henning.bull-berg@tsbw.cloud>
2020-09-29 12:53:15 +0200

    made that you can go to next quiz from picture

--- app/src/main/java/hkbb/de/pflanzenmemorie/quizPicture.java
+++ app/src/main/java/hkbb/de/pflanzenmemorie/quizPicture.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class quizPicture extends Fragment {
@@ -24,6 +26,10 @@ public class quizPicture extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        final DataViewModel model = new ViewModelProvider(requireActivity()).get(DataViewModel.class);
+
+        ImageView image = view.findViewById(R.id.plantPicture_pic);
+
         Button btn = view.findViewById(R.id.btn_picToQwest);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +38,19 @@ public class quizPicture extends Fragment {
             }
         });
 
+        Button btnNext = view.findViewById(R.id.btn_pictureToNextQuestion);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                model.setQuizPointer(model.getQuizPointer().getValue() + 1);
+                if (model.getQuizPointer().getValue() < model.getQuizSize().getValue()) {
+                    NavHostFragment.findNavController(quizPicture.this).navigate(R.id.action_quizPicture_self);
 
+                } else {
+                    NavHostFragment.findNavController(quizPicture.this).navigate(R.id.action_quizPicture_to_endStatistica);
+                }
+            }
+        });
 
     }
 }
